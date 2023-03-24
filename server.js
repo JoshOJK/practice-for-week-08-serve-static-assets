11d2634a8f17597734756fc7b0ea441a58b9dfeb
joshjk <jokrienke@comcast.net>
2023-03-23 22:01:41 -0700

    Phase2 complete

--- server.js
+++ server.js
@@ -1,8 +1,58 @@
 const http = require('http');
 const fs = require("fs");
 
+function getContentType(fileName) {
+  const ext = fileName.split(".")[1];
+  let contentType = 'text/plain';
+  switch (ext) {
+    case "jpg":
+    case "jpeg":
+      contentType = "image/jpeg";
+      break;
+    case "png":
+      contentType = "image/png";
+      break;
+    case "css":
+      contentType = "text/css";
+      break;
+    default:
+      contentType = "text/plain";
+      break;
+  }
+  return contentType;
+  // equivalent if-else-if statements:
+  // if (ext === 'jpg' || ext === 'jpeg') {
+  //   return "image/jpeg";
+  // } else if (ext === "png") {
+  //   return "image/png"
+  // } else {
+  //   return "text/plain"
+  // }
+}
+
+
 const server = http.createServer((req, res) => {
-  // Your code here
+      if (req.method === 'GET' && req.url.startsWith('/static') ) {
+        let splitPath = req.url.split('/static')[1]
+
+        try {
+          const fileGuy = fs.readFileSync(`./assets/${splitPath}`)
+          res.statusCode = 200;
+          res.setHeader('Content-Type', getContentType(splitPath));
+          return res.end(fileGuy);
+        } catch {
+          console.error(`cannot find asset ${splitPath} sucks to suck`);
+          res.statusCode = 404
+          return res.end()
+        }
+
+
+      }
+      const hello = fs.readFileSync("./index.html")
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(hello);
+
 });
 
 const port = 5000;
